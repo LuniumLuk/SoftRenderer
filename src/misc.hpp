e9c97d5a18f748db8829aa76b6d90ade86414954
Ziyi Lu <lunium1000@gmail.com>
2022-02-17 20:52:26 +0800

    Update misc.hpp

--- src/misc.hpp
+++ src/misc.hpp
@@ -9,8 +9,58 @@
 namespace Lurdr
 {
 
+#ifdef __CUSTOM_NEW__
+
+class MemoryCounter
+{
+private:
+    size_t m_newed_size;
+    size_t m_deleted_size;
+public:
+    MemoryCounter(): m_newed_size(0),
+                     m_deleted_size(0) {}
+    
+    void newed(size_t size)
+    {
+        m_newed_size += size;
+    }
+    void deleted(size_t size) 
+    {
+        m_deleted_size += deleted;
+    }
+
+    size_t currentAllocatedMemory()
+    {
+        return m_newed_size - m_deleted_size;
+    }
+    size_t totalNewedMemory()
+    {
+        return m_newed_size;
+    }
+    size_t totalDeletedMemory()
+    {
+        return m_deleted_size;
+    }
+};
+
+static MemoryCounter g_memory_counter = MemoryCounter();
+
+void* operator new(size_t size)
+{
+    g_memory_counter.newed(size);
+    printf("Current Allocated Memory (new) : %ld Bytes\n", g_memory_counter.currentAllocatedMemory());
+
+    return malloc(size);
+}
+
+#endif
+
 class Timer
 {
+private:
+    clock_t m_timer_start;
+    char*   m_timer_name;
+
 public:
     Timer(): m_timer_start(clock())
     {
@@ -37,10 +87,6 @@ public:
         printf("Timer (%s) : %.6fs (%.4fms)\n", m_timer_name, duration, duration * 1000);
         m_timer_start = 0;
     }
-
-private:
-    clock_t m_timer_start;
-    char * m_timer_name;
 };
     
 }
